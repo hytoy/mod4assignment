949a0246f3253272fb163c0bf13519fa17841c57
yedlarahulreddy <rahulreddy2208@gmail.com>
2025-03-31 20:44:25 +0530

    Add files via upload

---
+++ mainmod4.js
@@ -0,0 +1,11 @@
+var names = ["Robert", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim","john"];
+
+for(var i=0;i<names.length;i++){
+    var letter=names[i].charAt(0);
+    if(letter==='J'|| letter==='j'){
+        byespeaker.saygoodbye(names[i]);
+    }
+    else{
+        hellospeaker.sayhello(names[i]);
+    }
+}
---
+++ saygoodbye.js
@@ -0,0 +1,8 @@
+(function(window){
+    var goodbye="Goodbye";
+    var byespeaker={};
+    byespeaker.saygoodbye=function(names){
+        console.log(goodbye+" "+names);
+    }
+    window.byespeaker=byespeaker;
+})(window);
